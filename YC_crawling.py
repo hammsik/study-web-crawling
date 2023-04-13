1634695f6c4dd72c823c8f09fafb83e860e509f2
hammsik <asbbrp@gmail.com>
2023-04-14 02:29:05 +0900

    비디오완전로드 및 클릭 함수화, sleep 수정

--- YC_crawling.py
+++ YC_crawling.py
@@ -1,57 +1,65 @@
 from selenium import webdriver 
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By
 import time 
 
 driver = webdriver.Chrome('C:\WooooooooooW\etc\chromedriver_win32\chromedriver') 
-driver.get("https://www.youtube.com/feed/trending") 
-
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.by import By
+driver.get("https://www.youtube.com/results?search_query=s23") 
 html = driver.find_element(By.TAG_NAME, "html")
-prev_contents_len = 0 
-# while True: 
-#     time.sleep(3) 
-#     # contents = driver.find_elements(By.CSS_SELECTOR, '#items > ytd-video-renderer')
-#     contents = driver.find_elements(By.TAG_NAME, 'ytd-video-renderer')
 
-#     # contents = driver.find_elements(By.XPATH, "//html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-shelf-renderer[1]/div[1]/div[2]/ytd-vertical-list-renderer/div[1]/ytd-video-renderer")
-#     current_contents_len = len(contents)
-#     print(current_contents_len)
-#     if prev_contents_len == current_contents_len: 
-#         break 
-#     prev_contents_len = len(contents) 
-#     html.send_keys(Keys.END)
+def loadAllContents(): 
+    prev_contents_len = 0 
+    while True: 
+        time.sleep(2) 
+        contents = driver.find_elements(By.TAG_NAME, 'ytd-video-renderer') 
+        current_contents_len = len(contents) 
+        print("현재 로드된 영상 개수:", current_contents_len) 
+        if prev_contents_len == current_contents_len or current_contents_len > 100: 
+            break 
+        prev_contents_len = len(contents) 
+        html.send_keys(Keys.END) 
+    return contents
 
-#ActionChains생성
+def pickVideo():
+    cnt = 0
+    for c in contents:
+        time.sleep(1)
+
+        # 유튜브프리미엄 광고 팝업 닫기
+        try:    
+            html.find_element(By.XPATH, '//*[@id="dismiss-button"]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]').click()
+        except:
+            pass
+
+        # 동영상 클릭
+        toClick = c.find_element(By.TAG_NAME, "img")
+        action.move_to_element(toClick).click().perform()
+
+        time.sleep(2)
+        html.send_keys(Keys.END)
+        time.sleep(0.3)
+        html.send_keys(Keys.END)
+
+        time.sleep(1)
+        print("영상제목:", driver.find_element(By.XPATH, '//*[@id="title"]/h1/yt-formatted-string').text)
+        comments = driver.find_element(By.TAG_NAME, "ytd-comments")
+
+        try:
+            header = comments.find_element(By.XPATH, '//*[@id="count"]/yt-formatted-string/span[2]')
+            print("댓글 수:", header.text)
+        except:
+            print("댓글 없음")
+        print()
+
+        driver.back()
+        cnt += 1
+
+#원하는 영상으로 스크롤 후 클릭하기 위한 ActionChains 객체 생성
 action = ActionChains(driver)
-#리스트 가져오기
-contents = driver.find_elements(By.TAG_NAME, 'ytd-video-renderer')
-#move_to_element를 이용하여 이동
-time.sleep(2)
-print(len(contents))
-
-cnt = 0
-for c in contents:
-    time.sleep(2)
-    toClick = c.find_element(By.TAG_NAME, "img")
-    action.move_to_element(toClick).click().perform()
-    # 동영상 클릭
-    time.sleep(2)
-    html.send_keys(Keys.END)
-    time.sleep(0.5)
-    html.send_keys(Keys.END)
-    print(driver.find_element(By.XPATH, '//*[@id="title"]/h1/yt-formatted-string').text)
-    comments = driver.find_element(By.TAG_NAME, "ytd-comments")
-    time.sleep(2) 
-    try:
-        header = comments.find_element(By.XPATH, '//*[@id="count"]/yt-formatted-string/span[2]')
-        print("댓글 수:", header.text)
-    except:
-        print("댓글 없음")
-    print()
-
-    driver.back()
-    cnt += 1
+contents = loadAllContents()
+print("최종 로드된 영상 개수:", len(contents))
+pickVideo()
     #mmmmmmmmmmmmmmmm
     # if len(header) == 0: 
     #     driver.back() 
